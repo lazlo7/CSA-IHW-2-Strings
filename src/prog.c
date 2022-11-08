4cd092c30b7af20c6e6d40627f2d59c98e452ac2
Max Smagin <lazlogogalac777@gmail.com>
2022-11-09 00:20:31 +0300

    Restructure and change to c-strings

---
+++ src/prog.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#define STR_CAPACITY 10000
+
+void input_string(char* str) {
+    char c;
+    int i = 0;
+
+    while ((c = getchar()) != -1) {
+        assert(i + 1 < STR_CAPACITY && "Input string is too long");
+        str[i] = c;
+        ++i;
+    }
+
+    str[i] = '\0';
+}
+
+void print_string(const char* str) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        putchar(str[i]);
+    }
+}
+
+bool is_vowel(char c) {
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y';
+}
+
+void capitalize_consonants(char* str) {
+    for (int i = 0; str[i] != '\0'; ++i) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            if (!is_vowel(str[i])) {
+                str[i] -= 32;
+            }
+        }
+    }
+}
+
+int main() {
+    char str[STR_CAPACITY];
+    
+    input_string(str);
+    capitalize_consonants(str);
+    print_string(str);
+    putchar('\n');
+
+    return 0;
+}
